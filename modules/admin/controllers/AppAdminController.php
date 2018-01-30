d54f28c50e6eeaf88d6aed591a9d3db3504eee51
andriih <andrii.hnatyshyn@gmail.com>
2018-01-30 15:52:16 +0200

    order view added to admin section

--- modules/admin/controllers/AppAdminController.php
+++ modules/admin/controllers/AppAdminController.php
@@ -1,9 +1,8 @@
 <?php
 
 namespace app\modules\admin\controllers;
-
 use yii\web\Controller;
-use yii\filters\AccessControl;
+
 /**
  * Default controller for the `admin` module
  */
---
+++ modules/admin/models/Order.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\modules\admin\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "order".
+ *
+ * @property string $id
+ * @property string $created_at
+ * @property string $updated_at
+ * @property int $qty
+ * @property double $sum
+ * @property string $status
+ * @property string $name
+ * @property string $email
+ * @property string $phone
+ * @property string $address
+ */
+class Order extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'order';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['created_at', 'updated_at', 'qty', 'sum', 'name', 'email', 'phone', 'address'], 'required'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['qty'], 'integer'],
+            [['sum'], 'number'],
+            [['status'], 'string'],
+            [['name', 'email', 'phone', 'address'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'Номер замовлення',
+            'created_at' => 'Дата створення',
+            'updated_at' => 'Дата зміни',
+            'qty' => 'Кількість',
+            'sum' => 'Сума',
+            'status' => 'Статус',
+            'name' => 'Ім`я',
+            'email' => 'E-mail',
+            'phone' => 'Телефон',
+            'address' => 'Адрес',
+        ];
+    }
+}
