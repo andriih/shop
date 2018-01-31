3cded59c4f4ab9811394a5e4db13083281a1b823
andriih <andrii.hnatyshyn@gmail.com>
2018-01-31 21:34:28 +0200

    update form added

--- modules/admin/views/order/view.php
+++ modules/admin/views/order/view.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
@@ -12,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="order-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Перегляд замовлення №<?= $model->id ?></h1>
 
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
@@ -33,7 +34,12 @@ $this->params['breadcrumbs'][] = $this->title;
             'updated_at',
             'qty',
             'sum',
-            'status',
+            //'status',
+            [
+                    'attribute' => 'status',
+                    'value' => !$model->status ? '<span class="text-danger">Активний</span>' : '<span class="text-success">Завершений</span>',
+                    'format' => 'html'
+            ],
             'name',
             'email:email',
             'phone',
@@ -41,4 +47,31 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 
+    <?php $items = $model->orderItems; ?>
+
+    <div class="table-responsive">
+        <table class="table table-hover table-striped">
+            <thead>
+            <tr>
+                <th>Фото</th>
+                <th>Назва</th>
+                <th>Кільскість</th>
+                <th>Ціна</th>
+                <th>Сумма</th>
+                <th><span class="glyphicon glyphicon-remove" aria-heidden="true"></span></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($items as $item): ?>
+                <tr>
+                    <td><a href="<?= Url::to(['/product/view','id' => $item->product_id ]) ?>"><?= $item['name'] ?></a></td>
+                    <td><?= $item['qty_item'] ?></td>
+                    <td><?= $item['price'] ?></td>
+                    <td><?= $item['sum_item'] ?></td>
+                    <td><span data-id="<?=$id;?>" class="glyphicon glyphicon-remove text-danger del-item" aria-heidden="true"></span></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
