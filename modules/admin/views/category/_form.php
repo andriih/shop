3b68af3369fb4e37b7ad00b8d0decf4406f820bf
andriih <andrii.hnatyshyn@gmail.com>
2018-02-01 14:39:13 +0200

    update category form added

---
+++ modules/admin/views/category/_form.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\Category */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="category-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <? //echo $form->field($model, 'parent_id')->textInput(['maxlength' => true]) ?>
+	<?php echo $form->field($model,'parent_id')->dropDownList(ArrayHelper::map(app\models\Category::find()->all(), 'id',
+	'name')) ?>
+    
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'keywords')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -133,27 +133,21 @@ ltAppAsset::register($this);
 							</button>
 						</div>
 						<div class="mainmenu pull-left">
-							<ul class="nav navbar-nav collapse navbar-collapse">
-								<li><a href="index.html" class="active">Home</a></li>
-								<li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+                            <ul class="nav navbar-nav collapse navbar-collapse">
+                                <li><a href="<?=\yii\helpers\Url::to(['/admin']) ?>" class="active">Home</a></li>
+                                <li class="dropdown"><a href="#">Категорії<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="shop.html">Products</a></li>
-										<li><a href="product-details.html">Product Details</a></li> 
-										<li><a href="checkout.html">Checkout</a></li> 
-										<li><a href="cart.html">Cart</a></li> 
-										<li><a href="login.html">Login</a></li> 
+                                        <li><a href="<?=\yii\helpers\Url::to(['category/index']) ?>">Список категорій</a></li>
+                                        <li><a href="<?=\yii\helpers\Url::to(['category/create']) ?>">Додати категорію</a></li>
                                     </ul>
-                                </li> 
-								<li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
+                                </li>
+                                <li class="dropdown"><a href="#">Товари<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-										<li><a href="blog-single.html">Blog Single</a></li>
+                                        <li><a href="<?=\yii\helpers\Url::to(['product/index']) ?>">Список товарів</a></li>
                                     </ul>
-                                </li> 
-								<li><a href="404.html">404</a></li>
-								<li><a href="contact-us.html">Contact</a></li>
-							</ul>
-						</div>
+                                </li>
+                            </ul>
+                        </div>
 					</div>
 					<div class="col-sm-3">
 						<div class="search_box pull-right">
